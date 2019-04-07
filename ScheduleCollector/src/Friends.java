8d6f6d49fc7fb3b060a63dc614780cff0022370e
StefGoodwin <49354747+StefGoodwin@users.noreply.github.com>
2019-04-06 20:58:18 -0700

    Create Friends.java

---
+++ ScheduleCollector/src/Friends.java
@@ -0,0 +1,21 @@
+// Create the public class for the "Friends"
+
+public class Friends {
+	private String friendName;
+	private String[] friends = new String[3];
+	
+	public Friends(String friendName) {
+		this.friendName = friendName;
+	}
+	
+	public void addFriend(String friend) {
+		
+	}
+	
+	public String[] getFriend() {
+		return friends;
+	}
+	
+	 	
+	
+}
