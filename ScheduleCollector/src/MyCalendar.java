c8751046e02fae05d9487cc3fb1b8df11ee9b1e1
StefGoodwin <49354747+StefGoodwin@users.noreply.github.com>
2019-04-10 20:56:33 -0700

    Update MyCalendar.java

--- ScheduleCollector/src/MyCalendar.java
+++ ScheduleCollector/src/MyCalendar.java
@@ -1,36 +1,15 @@
 public class MyCalendar {
+	private String[] days = new String[7];
+	private String[] times = new String[17];
+	
+	public String[] getDays() {
+		return days;
+	}
+	
+	public String[] getTimes() {
+		return times;
+	}
 	
 	
-	 //create an array of days
-    String[] strDays = new String[]{
-                      "Sunday",
-                      "Monday", 
-                      "Tuesday",
-                      "Wednesday",
-                      "Thusday",
-                      "Friday",
-                      "Saturday"
-                    };
-	
-    //create an array of times
-    String[] strTimes = new String[]{
-                      "7 AM",
-                      "8 AM",
-                      "9 AM",
-                      "10 AM",
-                      "11 AM",
-                      "12 PM",
-                      "1 PM",
-                      "2 PM",
-                      "3 PM",
-                      "4 PM",
-                      "5 PM",
-                      "6 PM",
-                      "7 PM",
-                      "8 PM",
-                      "9 PM",
-                      "10 PM",
-                      "11 PM",
-                    };
 	
 }
